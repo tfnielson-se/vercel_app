af1abad45b626fa94dc19f5111f4a5b74b1f5560
Thomas <tfnielson.se@gmail.com>
2024-10-26 11:48:02 -0600

    fix vercel deployment issues const bcrypt

--- next_app/app/api/auth/login/route.ts
+++ next_app/app/api/auth/login/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-let bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 let jwt = require('jsonwebtoken');
 
 
